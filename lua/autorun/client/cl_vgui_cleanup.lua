4879e53bd1cd2243f5ce0b334533b3ae5ac7d786
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-06-06 17:47:33 +0400

    vgui_list

--- lua/autorun/client/cl_vgui_cleanup.lua
+++ lua/autorun/client/cl_vgui_cleanup.lua
@@ -30,4 +30,35 @@ do
         end
     end)
 
+    concommand.Add("vgui_list", function( ply, cmd, args )
+
+        local start_num = (args[1] and args[2]) and args[1] or 0
+        local end_num = args[2] or args[1]
+        local check = args[1] ~= nil or args[2] ~= nil
+
+        if (check) then
+            if (start_num) then
+                start_num = tonumber( start_num )
+            end
+
+            if (end_num) then
+                end_num = tonumber( end_num )
+            end
+        end
+
+        local num = 0
+        for key, value in pairs( debug_getregistry() ) do
+            if ispanel( value ) then
+                num = num + 1
+
+                if (check) then
+                    if (num < start_num) then continue end
+                    if (num > end_num) then break end
+                end
+
+                MsgC( "[", cmd, "] ", num, ". ", ColorRand(), value:GetName(), " (", value:GetClassName(), ")\n" )
+            end
+        end
+    end)
+
 end
